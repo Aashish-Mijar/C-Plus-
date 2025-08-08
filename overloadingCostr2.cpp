ceaec70d1f9c320a12d9c5e26c723de3f8238000
rep-demo <aashishmijar44@gmail.com>
2025-08-08 07:48:28 +0545

    Overloading constr file added

---
+++ overloadingCostr2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Home
+{
+private:
+	string name;
+
+public:
+	Home(string x=0)
+	{
+		name=x;
+	
+	}
+	
+	Home(Home &obj1)
+	{
+	name=obj1.name;
+	}
+	string getData()
+{
+	return name;
+}
+};
+int main()
+{
+	Home obj1("Aashish");
+	Home obj2(obj1);
+	cout<<obj2.getData();
+}
