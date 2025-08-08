1508f1801a891c6277ec336621da9e1604d7ffba
rep-demo <aashishmijar44@gmail.com>
2025-08-08 07:45:24 +0545

    Non parametrized constr added

---
+++ nonParametrizedConstr.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Circle
+{
+	private:
+		int radious=1;
+		public:
+			Circle()
+			{
+				radious=0;
+			}
+			int getData()
+			{
+				return 0;
+			}
+};
+ int main()
+{
+
+ Circle area();
+ cout<<area.getData();
+	
+	
+}
+	
