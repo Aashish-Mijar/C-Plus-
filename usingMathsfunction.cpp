9903e2436b9ec87d8a50d7fa433f5e886b99635a
rep-demo <aashishmijar44@gmail.com>
2025-08-09 22:48:45 +0545

    Math function file added

---
+++ usingMathsfunction.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double a=2.4, b=3.9,c;
+//	cout<<"Enter two numbers:"<<endl;
+//	cin>>a>>b;
+	
+//	c=max(a,b);
+//	c=min(a,b);
+//  c=pow(a,b);
+//	c=sqrt(b);
+//	c=abs(-a);
+//	c=ceil(a);
+//c=floor(a);
+//a=pow(a,2);
+//b=pow(b,2);
+c=sqrt(pow(a,2)+pow(b,2));
+	
+	cout<<"Value of c is "<<c<<endl;
+	return 0;
+	
+	
+}
