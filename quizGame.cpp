b011b60a20bfd9416d6ed638cf2931a30207813f
rep-demo <aashishmijar44@gmail.com>
2025-08-09 22:42:22 +0545

    Quiz game file added

---
+++ quizGame.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    string questions[]={"1. What year was c++ created?", 
+                         "2. Who invented c++?",
+                         "3. What is the predrecessor of c++?"    };
+                         
+    string options[][4]={{"A. 1986", "B. 1975", "C. 1985"},
+                         {"A. Denis Ritche", "B. Bjarne Stroustrup", "C. Mark Zuckerburg"},
+                         {"A. C", "B. Python", "C. C#"};
+    
+    string answerKey[]={"C", "B", "C"};
+    
+    
+    return 0;
+}
